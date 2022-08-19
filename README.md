6bbd94c868cc5a102576f20a5ac495e3a0c57ba7
rohit129 <rohit129.iiita@gmail.com>
2022-08-19 13:43:40 +0530

    Revert "First Commit till Layer Network"
    This reverts commit e71c5013c3ffdc238caee42fa2b8421cd85b3fa1.

--- README.md
+++
@@ -1,7 +0,0 @@
-## Learning to build Neural Networks from scratch using Python
-
--> Let's start this awesome journey.
-
-***Step-1:*** Create a Single Neuron.
-
-***Step-2:*** Create a Layered Architecture.
--- single_neuron.py
+++
@@ -1,8 +0,0 @@
-# Purpose: Single Neuron implementation using Python
-
-inputs = [1,2,3]
-weights = [0.2, 0.8, -0.5]
-bias = 2
-
-outputs = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
-print(outputs)
